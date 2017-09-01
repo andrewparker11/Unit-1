7e9a94ee1cc6a7fc764130ecdb10f3ee6b852201
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-01 14:45:30 -0400

    Updated from Brython Server: 9/1/2017 2:45:30 PM

--- nameAge.py
+++ nameAge.py
@@ -1,5 +1,8 @@
 #Andrew Parker
-#9/1/17
-#nameAge.py - Letters in your name and age next year
+#8/31/17
+#inputDemo.py - how to get user input
 
-
+name = input('What is your name? ')
+print('Hello', name)
+age = int(input('How old are you? '))
+print('You are', age+1, 'years old')
