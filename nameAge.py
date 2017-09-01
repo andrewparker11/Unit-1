d13f357d1f0e274101d2e66b5524b64818391a16
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-01 14:49:03 -0400

    Updated from Brython Server: 9/1/2017 2:49:03 PM

--- nameAge.py
+++ nameAge.py
@@ -2,7 +2,10 @@
 #8/31/17
 #inputDemo.py - how to get user input
 
-name = input('What is your name? ')
-print('Hello', name)
+fname = input('What is your first name? ')
+lname = input('What is your last name? ')
 age = int(input('How old are you? '))
-print('You are', age+1, 'years old')
+
+print(fname ,'has', len(fname), 'letters')
+print(lname ,'has', len(lname), 'letters')
+print('Next year you will be', age+1, 'years old')
