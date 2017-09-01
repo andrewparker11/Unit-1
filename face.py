3aa8ce892487e1e5aeab5f3c78ef08a0bd04a399
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-01 14:37:40 -0400

    Updated from Brython Server: 9/1/2017 2:37:40 PM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Andrew Parker
+#9/1/17
+#face.py - Displays a face
 
+print('    xxxxx    ')
+print('   x     x   ')
+print(' ((  o o  )) ')
+print('   |  v  |   ')
+print('   | === |   ')
+print('    -----    ')
