6406acf2d8774c3d82f17eab101a91546f01dc72
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-06 13:57:57 -0400

    Updated from Brython Server: 9/6/2017 1:57:56 PM

--- slope.py
+++ slope.py
@@ -2,4 +2,11 @@
 #8/31/17
 #slope.py - Calculates the slope ofe a line given two points
 
+x1 = float(input('What is your x1 value? '))
+y1 = float(input('What is your y1 value? '))
+x2 = float(input('What is your x2 value? '))
+y2 = float(input('What is your y2 value? '))
 
+print(fname ,'has', len(fname), 'letters')
+print(lname ,'has', len(lname), 'letters')
+print('Next year you will be', age+1, 'years old')
