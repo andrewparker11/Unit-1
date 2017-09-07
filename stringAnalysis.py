6a5e21a1afb00841df9def4624777785e3880573
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-07 14:51:39 -0400

    Updated from Brython Server: 9/7/2017 2:51:38 PM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -2,11 +2,9 @@
 #9/7/17
 #stringAnalysis.py - Gives word and character statistics of a given sentence
 
-sentence = input('Enter A Sentence')
+sentence = input('Enter A Sentence - ')
 characters = len(sentence)
-letters = characters-words
 words = sentence.count(' ')
+letters = characters-words
 
-
-
-print('Your sentence has', characters, 'characters,',  words+1, 'words' )
+print('Your sentence has', characters, 'characters,', letters, 'letters, and', words+1, 'words' )
