7226d8813d0aaf752f974a836eb14472a7654a72
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-07 14:20:07 -0400

    Updated from Brython Server: 9/7/2017 2:20:06 PM

--- warmup2.py
+++ warmup2.py
@@ -2,4 +2,10 @@
 #8/31/17
 #luckynumero - Finds your lucky numero
 
+from random import randint
 
+num1 = randint(1,10)
+num2 = randint(1,10)
+
+answer = int(input(str(num1)+ '+' + str(num2)+'? '))
+print(answer == num1 + num2)
