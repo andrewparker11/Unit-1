5d751d6e09bd4957457d580f07691cc2fa94440e
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-08 14:46:27 -0400

    Updated from Brython Server: 9/8/2017 2:46:27 PM

--- change.py
+++ change.py
@@ -1,1 +1,8 @@
+#Andrew Parker
+#9/8/17
+#change.py - Tells how many coins you need to make up a given change
 
+neggs = int(input('How many eggsys? '))
+ncartons = int(input('How many cartons? '))
+
+print(neggs < (ncartons*12))
