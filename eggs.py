f9464eaa3f2e0a23d94ba8f9218d137d59abfddf
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-08 14:30:27 -0400

    Updated from Brython Server: 9/8/2017 2:30:26 PM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,11 @@
+#Andrew Parker
+#9/8/17
+#eggs.py - 
+
+x1 = float(input('What is your x1 value? '))
+y1 = float(input('What is your y1 value? '))
+x2 = float(input('What is your x2 value? '))
+y2 = float(input('What is your y2 value? '))
+
+print('The slope of your line is', (y1-y2)/(x1-x2))
 
