ae74669b93b24692a70b960675031addaf7d101f
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-11 14:23:07 -0400

    Update quiz1.py

--- quiz1.py
+++ quiz1.py
@@ -2,4 +2,10 @@
 #9/11/17
 #quiz1.py - First Quiz
 
+
 name = input('What is your name? ') 
+n1 = int(input('Enter a number '))
+n2 = int(input('Enter a number '))
+print('The sum is', n1+n2)
+print('The product is', n1*n2)
+print('Your Lucky Number is', (n1*7)+(n2*7))
